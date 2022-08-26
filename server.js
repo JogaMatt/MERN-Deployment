40dfdf35a6518cda010585a8ea2b0f9931c5770c
DESKTOP-UK2RI9P\Matt Joga <matthewjoga@outlook.com>
2022-08-26 13:08:44 -0400

    First Heroku Commit

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 8000;
+const localPort = 8000;
+require('dotenv').config();
 
 require('./server/config/mongoose.config');
 
@@ -11,4 +12,4 @@ app.use(express.urlencoded({ extended: true }));
 require('./server/routes/card.routes')(app);
 require('./server/routes/pokedex.routes')(app);
 
-app.listen(port, () => console.log(`We're up and rolling on port ${port}!`));
+app.listen(process.env.PORT || localPort, () => console.log(`We're up and rolling on port ${port}!`));
