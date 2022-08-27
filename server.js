1a9d2a402cb9493a764dfe0fafcff7b8f0c44f4a
DESKTOP-UK2RI9P\Matt Joga <matthewjoga@outlook.com>
2022-08-27 07:21:34 -0400

    Version 1.0.0

--- server.js
+++ server.js
@@ -3,14 +3,27 @@ const cors = require('cors');
 const app = express();
 const localPort = 8000;
 require('dotenv').config();
+const path = require('path')
 
-require('./config/mongoose.config');
+require('./server/config/mongoose.config');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-require('./routes/card.routes')(app);
-require('./routes/pokedex.routes')(app);
+
+// USE ROUTES
+require('./server/routes/card.routes')(app);
+require('./server/routes/pokedex.routes')(app);
+
+// Server static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const herokuPort = process.env.PORT
 console.log(herokuPort)
