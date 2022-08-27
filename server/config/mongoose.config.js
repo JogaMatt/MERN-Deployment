7859e7d4c0f42ce4e32218b6b0e3c437b3bd4818
DESKTOP-UK2RI9P\Matt Joga <matthewjoga@outlook.com>
2022-08-27 07:42:51 -0400

    Version 1.0.2

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 const database = 'cards'
+const CONNECTION_URL = 'mongodb+srv://JogaMatt:LilMan315@cluster0.i3421pe.mongodb.net/?retryWrites=true&w=majority'
 
-mongoose.connect('mongodb://127.0.0.1:27017/' + database, {
+
+mongoose.connect(CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
     .then(() => console.log(`Established a connection to the ${database} database!`))
     .catch(err => console.log(`Something went wrong: ${err}`));
+
+mongoose.set('useFindAndModify', false)
